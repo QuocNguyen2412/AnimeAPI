4d2a1dbc0a0bf2fc2c724c67248eec25af24bf8b
QuocNguyen2412 <dannynguyen892412@gmail.com>
2022-08-08 16:48:12 -0700

    commit

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// const { request, response } = require('express')
+// const express = require('express')
+// const app = express()
+// const PORT = 8000
+
+// app.get('/', (request, response)=> {
+//   response.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/api', (request, response)=>{
+//   response.json()
+// })
+
+// app.listen(PORT, ()=>{
+//   console.log(`the server is running on ${PORT}`)
+// })
+ 
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
@@ -6,9 +24,12 @@ function getFetch(){
   fetch(url)
       .then(res => res.json())
       .then(data => {
-        console.log(data.data[0]) 
-        data.data[0].genres.forEach( obj => {
-          document.querySelector('.GE').innerText = obj.name
+        console.log(data.data[0].genres) 
+        data.data[0].genres.forEach( obj => { 
+          let p = document.createElement('p')
+          p.textContent = obj.name
+            document.querySelector('#one').appendChild(p)
+          
         })
        document.querySelector('.DE').innerText = data.data[0].synopsis
        document.querySelector('img').src = data.data[0].images.jpg.image_url
